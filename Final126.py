47d2f9f538a6b986c6f0bc8cfe1efc3a4ddb7aae
Marzmans <marzan.mk@gmail.com>
2023-12-15 14:33:58 -0500

    Create Final126.py

---
+++ Final126.py
@@ -0,0 +1,26 @@
+'''
+Checklist items that I completed in consolidation project:
+1.6 - You can run a Python script from a command line using additional arguments.
+3.17 - Imported a module I created myself.
+3.18 - I used sys.argv for user input at command line
+3.20 - Wrote results of program out to a file
+3.21 - In my with open statement in my ciphers, I used append mode on 'with open statement' to append results of the program to a file
+4.9 - raised an error and provided a helpful error message to the user
+5.7 - Creating a List Manually
+5.8 - Appending an item to a list
+5.10 - Removing an item from a list
+5.15 - Creating a dictionary manually
+5.17 - Accessing Keys of a dictionary
+5.18 - Iterating through items of a dictionary
+5.19 - Updating values in dictionairy systematically
+6.1 - I had a README for the last project
+6.2 - I wrote documentation comments at the top of my script
+6.5 - I wrote sufficient and useful documentation
+6.7 - I chose an appropriate license for my last project (MIT)
+Git - I completed all of the Git requirements for the last project
+
+To Do in This Project: 
+NumPy and Pandas 8.1-4
+Webscraping and JSON - 10.4 and 10.5
+3.19, 5.9, 5.11, 5.12, 5.13, 5.14
+'''
